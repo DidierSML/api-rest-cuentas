56d06fc6b58d62282b3b82efeff0f436823b0e56
DidierSML <didiersmaldonad@gmail.com>
2024-04-12 17:02:16 -0500

    Adding HATEOAS functions in order to generate Links that show up -cuentaId- and -obtenerCuentas-

--- src/main/java/com/api/hateoas/controller/CuentaController.java
+++ src/main/java/com/api/hateoas/controller/CuentaController.java
@@ -3,10 +3,17 @@ package com.api.hateoas.controller;
 import com.api.hateoas.model.Cuenta;
 import com.api.hateoas.service.CuentaService;
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @AllArgsConstructor
@@ -20,20 +27,63 @@ public class CuentaController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cuenta guardarCuenta (@RequestBody Cuenta cuentaNueva){
 
+        //Llamamos el método -guardarCuenta- del servicio y lo asignamos a una variable
+        Cuenta cuentaHateoas = cuentaService.guardarCuenta(cuentaNueva);
+
+        //Haciendo uso de la variable generamos 2 enlaces;
+        //El Primero direccionará a la cuenta específica por su id
+        cuentaHateoas.add(linkTo(methodOn(CuentaController.class).obtenerCuentaPorId(cuentaHateoas.getId())).withSelfRel());
+        //El Segundo direccionará de manera dinámica al método que Colecciona todas las cuentas de forma dinámica
+        cuentaHateoas.add(linkTo(WebMvcLinkBuilder.methodOn(CuentaController.class).obtenerCuentas()).withRel(IanaLinkRelations.COLLECTION));
+
+        //Finalmente se retornará la cuenta guardada + los dos enlaces anteriores
         return cuentaService.guardarCuenta(cuentaNueva);
     }
 
     //Obtener/Listar todas las cuentas
     @GetMapping("obtenerCuentas")
     @ResponseStatus(HttpStatus.OK)
-    public List<Cuenta> obtenerCuentas (){
-        return  cuentaService.obtenerCuentas();
+    public List <Cuenta> obtenerCuentas (){
+
+        //Línea que llama al Servicio
+        List <Cuenta> cuentas = cuentaService.obtenerCuentas();
+
+        //Creamos un ciclo for para iterar sobre las -cuentas almacenadas-
+        for(Cuenta cuenta: cuentas){
+
+            //Creamos una lista que va a guardar los enlaces que incluiremos en el GET como respuesta
+            List <Link> enlaces = new ArrayList<>();
+
+            //La lista -enlaces- agregará su primer link: cuenta por cada -id- con direccionamiento a este mismo método
+            enlaces.add(linkTo(methodOn(CuentaController.class).obtenerCuentaPorId(cuenta.getId())).withSelfRel());
+
+            //Este enlace apunta a la Colección completa de -cuentas- de manera dinámica, es más flexible si se presentan cambios futuros
+            enlaces.add(linkTo(WebMvcLinkBuilder.methodOn(CuentaController.class).obtenerCuentas()).withRel(IanaLinkRelations.COLLECTION));
+
+            //Aquí agregamos los enlaces anteriores a la lista cuenta del bucle for-each
+            cuenta.add(enlaces);
+        }
+
+        //Finalmente, retornamos la lista -cuentas- que ahora incluye los enlaces obtenidos en el anterior bucle
+        return  cuentas;
     }
 
     //Obtener cuenta por Id
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
     public Cuenta obtenerCuentaPorId (@PathVariable (value = "id") Integer id){
+
+        //Creamos una variable que guardará el llamado al método -obtenerCuentaPorId- del servicio
+        Cuenta cuentaHateoas = cuentaService.obtenerCuentaPorId(id);
+
+        //Enlace que apunta al -Id de la Cuenta-
+        cuentaHateoas.add(linkTo(WebMvcLinkBuilder.methodOn(CuentaController.class).obtenerCuentaPorId(cuentaHateoas.getId())).withSelfRel());
+        //Enlace Dinámico -obtenerCuentas
+        cuentaHateoas.add(linkTo(WebMvcLinkBuilder.methodOn(CuentaController.class).obtenerCuentas()).withRel(IanaLinkRelations.COLLECTION));
+        //Enlace Estático -obtenerCuentas
+        cuentaHateoas.add(linkTo(WebMvcLinkBuilder.methodOn(CuentaController.class).obtenerCuentas()).withSelfRel());
+
+        //Retornamos la -cuenta por Id-
         return cuentaService.obtenerCuentaPorId(id);
     }
 
@@ -41,6 +91,15 @@ public class CuentaController {
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
     public Cuenta acutalizarCuenta (@PathVariable (value = "id") Integer id, @RequestBody Cuenta cuentaExistente){
+
+        //Creamos una variable que guardará el llamado al método -actualizarCuenta- del servicio
+        Cuenta cuentaHateoas = cuentaService.acutalizarCuenta(id, cuentaExistente);
+
+        //Obtenemos el enlace de la cuenta actualizada y de las cuentas en general
+        cuentaHateoas.add(linkTo(methodOn(CuentaController.class).obtenerCuentaPorId(cuentaHateoas.getId())).withSelfRel());
+        cuentaHateoas.add(linkTo(WebMvcLinkBuilder.methodOn(CuentaController.class).obtenerCuentas()).withRel(IanaLinkRelations.COLLECTION));
+
+        //Retornamos la cuentaActualizada
         return cuentaService.acutalizarCuenta(id,cuentaExistente);
     }
 
@@ -48,6 +107,19 @@ public class CuentaController {
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void eliminarCuenta (@PathVariable (value = "id") Integer id){
+
         cuentaService.eliminarCuenta(id);
+
     }
 }
+
+/**
+ * Cabe resaltar que al momento de usar enlaces lo podemos hacer de forma dinámica o estática, ambos son útiles:
+ *
+ * (Ejemplo): Para apuntar a la lista total de -cuentas- se puede hacer de estas formas:
+ *
+ *     (Este enlace apunta a la lista General de -cuentas- de manera estática con un link sencillo)
+ *      ->     enlaces.add(linkTo(WebMvcLinkBuilder.methodOn(CuentaController.class).obtenerCuentas()).withSelfRel());
+ *     (Este enlace apunta a la Colección completa de -cuentas- de manera dinámica, es más flexible si se presentan cambios futuros)
+ *      ->    enlaces.add(linkTo(WebMvcLinkBuilder.methodOn(CuentaController.class).obtenerCuentas()).withRel(IanaLinkRelations.COLLECTION));
+ */
